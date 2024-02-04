9504075b5f739f4da94db1bb4b0741ab70065c8a
Vacuum Bubble <andrearusso.quant@gmail.com>
2024-02-04 11:29:09 +0000

    Update Mean Variance Portfolio optimisation.py

--- Monte Carlo and Option Pricing/Mean Variance Portfolio optimisation.py
+++ Monte Carlo and Option Pricing/Mean Variance Portfolio optimisation.py
@@ -35,18 +35,19 @@ import yfinance as yf
 
 # Plotting
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 #%% Retrieve data
 
 # We will use chips stocks to construct our porfolio
-tickers = ['KWEB', 'VZ', 'NEM', 'TSN', 'SBUX']
+tickers = ['NEM', 'SBUX', 'AMD', 'VZ', 'AVGO']
 
 num_asset = len(tickers)
 num_simul = 20000
 
 # Fetching data from yfinance (disable progeress bar)
 start_date = '2015-01-01'
-end_date = '2024-01-01'
+end_date = '2020-01-01'
 stocks = yf.download(tickers, start=start_date, 
                           end=end_date, progress=True)['Adj Close']
 
@@ -100,12 +101,36 @@ annual_log_returns = log_returns.mean() * 252
 print(f"Annualised percentage returns: {annual_returns}")
 print(f"Annualised log-returns: {annual_log_returns}")
 
-fig = plt.figure()
-ax = plt.axes()
-ax.bar(annual_log_returns.index, annual_log_returns*100, color='r', alpha=0.8)
-ax.set_title('Annualised log-returns (in %)')
+# Visualise Annualized Log-Returns
+fig, ax = plt.subplots(figsize=(10, 6))  
+colors = plt.cm.viridis(np.linspace(0, 1, len(annual_log_returns)))  
+bars = ax.bar(annual_log_returns.index, annual_log_returns*100, color=colors, alpha=0.8)
 
-""" we proceed using log-returns"""
+# Adding value labels on top of each bar for precise values
+for bar in bars:
+    height = bar.get_height()
+    ax.annotate(f'{height:.2f}%',
+                xy=(bar.get_x() + bar.get_width() / 2, height),
+                xytext=(0, 3), 
+                textcoords="offset points",
+                ha='center', va='bottom')
+
+# Plot details
+ax.set_title('Annualized Log-Returns (in %)', fontsize=14, fontweight='bold')
+ax.set_ylabel('Log-Returns (%)', fontsize=12)
+ax.set_xlabel('Assets', fontsize=12)
+ax.grid(True, which='major', linestyle='--', linewidth='0.5', color='grey')
+ax.set_axisbelow(True)
+
+# Format the y-axis to show percentage
+ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+# Rotate x-axis tick labels for better readability
+plt.xticks(rotation=45)
+
+plt.tight_layout()  # Adjust layout to make room for the rotated x-axis labels
+plt.show()
+
 
 #%% Compute historical volatility
 vols = log_returns.std()
@@ -113,12 +138,37 @@ vols = log_returns.std()
 # Calculate annualized volatilities
 annual_vols = vols * np.sqrt(252)
 
-# Visualize the data
-fig = plt.figure()
-ax = plt.axes()
+# Visualization of Annualized Volatilities
+fig, ax = plt.subplots(figsize=(10, 6)) 
 
-ax.bar(annual_vols.index, annual_vols*100, color='orange', alpha=0.5)
-ax.set_title('Annualized Volatility (in %)');
+# Plotting the bar chart with a more appealing color scheme
+colors = plt.cm.inferno(np.linspace(0, 1, len(annual_vols))) 
+bars = ax.bar(annual_vols.index, annual_vols*100, color=colors, alpha=0.8)
+
+# Adding value labels on top of each bar for precise values
+for bar in bars:
+    height = bar.get_height()
+    ax.annotate(f'{height:.2f}%',
+                xy=(bar.get_x() + bar.get_width() / 2, height),
+                xytext=(0, 3),  
+                textcoords="offset points",
+                ha='center', va='bottom')
+
+# Plot details
+ax.set_title('Annualized Volatility (in %)', fontsize=14, fontweight='bold')
+ax.set_ylabel('Volatility (%)', fontsize=12)
+ax.set_xlabel('Assets', fontsize=12)
+ax.grid(True, which='major', linestyle='--', linewidth='0.5', color='grey')
+ax.set_axisbelow(True)
+
+# Format the y-axis to show percentage
+ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+# Rotate x-axis tick labels for better readability
+plt.xticks(rotation=45)
+
+plt.tight_layout()  
+plt.show()
 
 #%% Portfolio statistics
 
@@ -232,7 +282,6 @@ ax.set_title('Monte Carlo Simulated Allocation')
 
 
 # Scatter plot for the portfolios
-# Color by Sharpe ratio; this time using actual returns
 scatter = ax.scatter(portfolio_vols, portfolio_returns, c=portfolio_sharpe,
                      cmap='RdYlGn', marker='o', edgecolor='k', alpha=0.7)
 
@@ -247,12 +296,14 @@ ax.scatter(max_sharpe['volatility'], max_sharpe['log-returns'],
 # Labels and title
 ax.set_title('Simulated Portfolios with Log-Returns', fontsize=14)
 ax.set_xlabel('Expected Volatility', fontsize=12)
-ax.set_ylabel('Expected Actual Return', fontsize=12)
+ax.set_ylabel('Expected Log-Return', fontsize=12)
 ax.grid(True)
 ax.legend()
 
 #%% Visualise actual returns
 
+"""
+OPTIONAL
 # Conversion of annualized log returns to annualized actual returns
 annualized_actual_returns = np.exp(portfolio_returns) - 1
 
@@ -285,21 +336,26 @@ ax.legend()
 
 plt.show()
 
-#%% Max-Sharpe ration prtfolio
+"""
+
+#%% Max-Sharpe ratio prtfolio
 """
 We use the scipy optimisation module to find the portfolio 
 with the maximum sharpe ratio
 """
-
-# Maximizing sharpe ratio
-def min_sharpe_ratio(weights, annual_log_returns, annual_covar):
-    return -portfolio_stats(weights, annual_log_returns, annual_covar)[2]
 
 # Constraints to ensure the sum of weights is 1 (fully invested portfolio)
 cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
 
 # Bounds to ensure each weight is between 0 and 1
 bnds = tuple((0, 1) for _ in range(num_asset))
+
+
+
+# Maximizing sharpe ratio
+def min_sharpe_ratio(weights, annualised_returns, annualised_covar):
+    return -portfolio_stats(weights, annualised_returns, annualised_covar)[2]
+
 
 # Initial weights (equally distributed)
 initial_wts = np.array(num_asset * [1. / num_asset])
@@ -309,10 +365,10 @@ opt_sharpe = sco.minimize(lambda weights: min_sharpe_ratio(weights, annual_log_r
                           initial_wts, method='SLSQP', bounds=bnds, constraints=cons)
 
 
-optimized_weights = np.around(opt_sharpe.x * 100, 2)
+optimized_weights = np.around(opt_sharpe['x'] * 100, 2)
 ticker_weights = list(zip(tickers, optimized_weights))
 
-print("Optimized Portfolio Weights:")
+print("Max Sharpe Optimized Portfolio Weights:")
 for ticker, weight in ticker_weights:
     print(f"{ticker}: {weight}%")
 
@@ -321,9 +377,76 @@ optimized_portfolio_stats = portfolio_stats(opt_sharpe.x, annual_log_returns, an
 stats_labels = ['Returns', 'Volatility', 'Sharpe Ratio']
 optimized_stats = np.around(optimized_portfolio_stats, 4)
 
-print("Optimized Portfolio Statistics:")
+print("Max Sharpe Optimized Portfolio Statistics:")
 for stat, value in zip(stats_labels, optimized_stats):
     print(f"{stat}: {value}")
     
 #%% Min variance portfolio
 
+def min_variance(weights, annualised_returns, annualised_covar):
+    return portfolio_stats(weights, annualised_returns, annualised_covar)[1]**2
+
+# Optimising for minimum variance
+opt_var = sco.minimize(lambda weights: min_variance(weights, annual_log_returns, annual_covar), 
+                          initial_wts, method='SLSQP', bounds=bnds, constraints=cons)
+
+optimized_weights_2 = np.around(opt_var['x'] * 100, 2)
+ticker_weights_2 = list(zip(tickers, optimized_weights_2))
+
+print("Min Variance Portfolio Weights:")
+for ticker, weight in ticker_weights_2:
+    print(f"{ticker}: {weight}%")
+
+# Portfolio stats
+optimized_portfolio_stats_2 = portfolio_stats(opt_var['x'], annual_log_returns, annual_covar)
+stats_labels = ['Returns', 'Volatility', 'Sharpe Ratio']
+optimized_stats_2 = np.around(optimized_portfolio_stats_2, 4)
+
+print("Min Variance Portfolio Statistics:")
+for stat, value in zip(stats_labels, optimized_stats_2):
+    print(f"{stat}: {value}")
+    
+#%% Efficient Frontier Portfolio
+
+# Minimize the volatility
+def min_volatility(weights, annualised_returns, annualised_covar):
+    return portfolio_stats(weights, annualised_returns, annualised_covar)[1]
+
+target_rets = np.linspace(0.1, 0.6, 200)
+target_vols = []
+
+for t_ret in target_rets:
+    ef_cons = ({'type': 'eq', 'fun': lambda x: portfolio_stats(x, annual_log_returns, annual_covar)[0] - t_ret},
+               {'type': 'eq', 'fun': lambda x: np.sum(x) -1 })
+    opt_ef = sco.minimize(lambda weights: min_volatility(weights, annual_log_returns, annual_covar),
+                          initial_wts, method='SLSQP', bounds=bnds, constraints=ef_cons)
+    
+    target_vols.append(opt_ef['fun'])
+
+target_vols = np.array(target_vols)
+
+#%% Visualise Efficient Frontier
+fig, ax = plt.subplots(figsize=(10, 6))
+
+
+# Scatter plot for the Efficient Frontier
+scatter = ax.scatter(target_vols, target_rets, c=target_rets / target_vols, cmap='RdYlGn', marker='.', edgecolors='k', alpha=0.7)
+colorbar = fig.colorbar(scatter, ax=ax)
+colorbar.set_label('Sharpe Ratio (Color)')
+
+# Maximum Sharpe Ratio Portfolio
+max_sharpe_stats = portfolio_stats(opt_sharpe.x, annual_log_returns, annual_covar)
+ax.scatter(max_sharpe_stats[1], max_sharpe_stats[0], color='red', s=100, edgecolors='black', label='Max Sharpe Ratio Portfolio', zorder=5)
+
+# Minimum Variance Portfolio
+min_var_stats = portfolio_stats(opt_var.x, annual_log_returns, annual_covar)
+ax.scatter(min_var_stats[1], min_var_stats[0], color='blue', s=100, edgecolors='black', label='Min Variance Portfolio', zorder=5)
+
+# Labels and title
+ax.set_title('Efficient Frontier', fontsize=14)
+ax.set_xlabel('Expected Volatility', fontsize=12)
+ax.set_ylabel('Expected Return', fontsize=12)
+ax.grid(True)
+ax.legend()
+
+plt.show()
